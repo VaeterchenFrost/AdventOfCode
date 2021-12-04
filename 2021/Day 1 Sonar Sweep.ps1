c6e77c1be74e85dd7454e27b3b149cc37e25c1f0
Martin Röbke <martin.roebke@web.de>
2021-12-04 15:26:17 +0100

    Day 1 and 2

---
+++ 2021/Day 1 Sonar Sweep.ps1
@@ -0,0 +1,56 @@
+<# 
+--- Day 1: Sonar Sweep ---
+
+Count the number of times a depth measurement increases from the previous measurement. 
+There is no measurement before the first measurement. In the example above, the changes are as follows:
+199 (N/A - no previous measurement)
+200 (increased)
+208 (increased)
+210 (increased)
+200 (decreased)
+207 (increased)
+240 (increased)
+269 (increased)
+260 (decreased)
+263 (increased) 
+#>
+
+$folder = "C:\Users\Martin\OneDrive\Informatik\AdventOfCode\2021\"
+$file = "input1"
+
+$measurements = Get-Content(Get-ChildItem ($folder + $file)) | foreach { [int]$_ }
+
+# $increased = 0
+# foreach ($index in (0..($measurements.Length - 2))) {
+#     $increased += ($measurements[$index + 1] -gt $measurements[$index] )
+# }
+# write $increased
+
+<#
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. 
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+#>
+$sliding_window = [int[]]::new($measurements.Count - 2)
+foreach ($index in (0..($measurements.Count - 3))) {
+    $sliding_window[$index] = $measurements[$index] + $measurements[$index + 1] + $measurements[$index + 2] 
+}
+$increased = 0
+foreach ($index in (0..($sliding_window.Length - 2))) {
+    $increased += ($sliding_window[$index + 1] -gt $sliding_window[$index] )
+}
+write $increased
---
+++ 2021/Day 2 Dive.ps1
@@ -0,0 +1,66 @@
+<# 
+--- Day 2: Dive! ---
+
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+
+    forward X increases the horizontal position by X units.
+    down X increases the depth by X units.
+    up X decreases the depth by X units.
+#>
+
+$folder = "C:\Users\Martin\OneDrive\Informatik\AdventOfCode\2021\"
+$file = "input2"
+
+$instructions = Get-Content(Get-ChildItem ($folder + $file))
+$horizontal = $depth = 0
+
+# foreach ($instruction in $instructions) {
+#     $val = $instruction -split " "
+#     if ( $val[0] -eq "forward" ) { $horizontal += $val[1] }
+#     elseif ( $val[0] -eq "down" ) { $depth += $val[1] }
+#     elseif ($val[0] -eq "up" ) { $depth -= $val[1] }
+#     else { Write-Output "error in instruction $instruction" }
+# }
+# write ($horizontal * $depth)
+
+<#In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+    forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    down 5 adds 5 to your aim, resulting in a value of 5.
+    forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    up 3 decreases your aim by 3, resulting in a value of 2.
+    down 8 adds 8 to your aim, resulting in a value of 10.
+    forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+#>
+
+$aim = 0
+
+foreach ($instruction in $instructions) {
+    $val = $instruction -split " "
+    if ( $val[0] -eq "forward" ) { $horizontal += $val[1]; $depth += $aim * $val[1] }
+    elseif ( $val[0] -eq "down" ) { $aim += $val[1] }
+    elseif ($val[0] -eq "up" ) { $aim -= $val[1] }
+    else { Write-Output "error in instruction $instruction" }
+}
+write ($horizontal * $depth)

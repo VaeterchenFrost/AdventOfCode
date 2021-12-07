61b6740721badcf8466589f0ae752c908ba385d0
Martin Röbke <martin.roebke@web.de>
2021-12-08 00:15:27 +0100

    Create Day 5 Hydro Venture.ps1

---
+++ 2021/Day 5 Hydro Venture.ps1
@@ -0,0 +1,30 @@
+<#
+Day 5: Hydrothermal Venture
+
+Each line of vents is given as a line segment in the format x1,y1 -> x2,y2 where x1,y1 are the coordinates of one end 
+of the line segment and x2,y2 are the coordinates of the other end.
+These line segments include the points at both ends.
+
+Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
+#>
+
+$file = $PSScriptRoot + "/input5"
+$lines = Get-Content(Get-ChildItem ($file))
+
+$numbers = $lines | % { ( $_ | Select-String "(\d+)" -AllMatches).Matches.Value | % { [int]$_ } }
+$min, $max = $numbers | Sort-Object | Select-Object -First 1 -Last 1
+
+$field = New-Object 'int[,]' ($max+1), ($max+1)
+
+foreach ($index in (0..($lines.Count - 1))) { 
+    $x1, $y1, $x2, $y2 = $numbers[(4 * $index)..(4 * $index + 3)]; 
+    Write-Debug "$x1,$y1->$x2,$y2" 
+    if ($x1 -eq $x2 -or $y1 -eq $y2) {
+        if ($x1 -eq $x2) {
+            ($y1..$y2).ForEach({ $field[$x1, $_]++ })
+        }
+        if ($y1 -eq $y2) {
+            ($x1..$x2).ForEach({ $field[$_, $y1]++ })
+        }
+    }
+}

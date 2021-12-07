cd2ee4c58ca8ff4adc0a33a6c367f1c5f1456d68
Martin Röbke <martin.roebke@web.de>
2021-12-08 00:39:49 +0100

    Complete Day 5

--- 2021/Day 5 Hydro Venture.ps1
+++ 2021/Day 5 Hydro Venture.ps1
@@ -14,17 +14,23 @@ $lines = Get-Content(Get-ChildItem ($file))
 $numbers = $lines | % { ( $_ | Select-String "(\d+)" -AllMatches).Matches.Value | % { [int]$_ } }
 $min, $max = $numbers | Sort-Object | Select-Object -First 1 -Last 1
 
-$field = New-Object 'int[,]' ($max+1), ($max+1)
+$field = New-Object 'int[,]' ($max + 1), ($max + 1)
 
 foreach ($index in (0..($lines.Count - 1))) { 
     $x1, $y1, $x2, $y2 = $numbers[(4 * $index)..(4 * $index + 3)]; 
-    Write-Debug "$x1,$y1->$x2,$y2" 
-    if ($x1 -eq $x2 -or $y1 -eq $y2) {
-        if ($x1 -eq $x2) {
-            ($y1..$y2).ForEach({ $field[$x1, $_]++ })
-        }
-        if ($y1 -eq $y2) {
-            ($x1..$x2).ForEach({ $field[$_, $y1]++ })
-        }
+    # Write-Debug "$x1,$y1->$x2,$y2" # uncomment for Part 1:
+    # if ($x1 -eq $x2 -or $y1 -eq $y2) {
+    if ($x1 -eq $x2) {
+        ($y1..$y2).ForEach({ ++$field[$x1, $_] })
     }
+    elseif ($y1 -eq $y2) {
+        ($x1..$x2).ForEach({ ++$field[$_, $y1] })
+    }
+    else {
+        $xrange = ($x1..$x2)
+        $yrange = ($y1..$y2)
+        (1..$xrange.count).ForEach({ ++$field[$xrange[$_ - 1], $yrange[$_ - 1]] })
+    }
+    # }
 }
+Write-Warning $field.Where({ $_ -ge 2 }).count 

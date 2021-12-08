38c282355573dfedbcbbf8aad6c4578f3f122d06
Martin Röbke <martin.roebke@web.de>
2021-12-08 01:17:18 +0100

    Complete Day 6 Lanternfish

--- 2021/Day 6 Lanternfish.ps1
+++ 2021/Day 6 Lanternfish.ps1
@@ -7,15 +7,19 @@ Initial state: 3,4,3,1,2
 In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of 5934.
 
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
+--- Part Two ---
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+How many lanternfish would there be after 256 days?
 #>
 
 $file = $PSScriptRoot + "/input6"
 $numbers = [System.IO.File]::ReadAllText((Get-ChildItem $file)) -split "," | ForEach-Object { [int]$_ }
 #$numbers = @(1)
 
-$fishes_per_day = [int[]]::new(9)
+$fishes_per_day = [bigint[]]::new(9)
 $numbers | Group-Object | ForEach-Object { $fishes_per_day[$_.Name] = $_.count }
-(1..80).ForEach({
+(1..256).ForEach({
         $spawn = $fishes_per_day[0]
         (0..($fishes_per_day.Count - 2)) | ForEach-Object { $fishes_per_day[$_] = $fishes_per_day[$_ + 1] }
         $fishes_per_day[8] = $spawn

f1c5e2522909352e0dd431f56d965c6bc98281bf
Martin Röbke <martin.roebke@web.de>
2021-12-10 22:14:10 +0100

    Complete 2021 day 10

--- 2021/Day 10 Syntax Scoring.ps1
+++ 2021/Day 10 Syntax Scoring.ps1
@@ -20,7 +20,7 @@ take the first illegal character on the line and look it up in the following tab
   }: 1197 points.
   >: 25137 points.
 Find the first illegal character in each corrupted line of the navigation subsystem. What is the total syntax error score for those errors?#>
-# Import-Module functional
+Import-Module functional
 
 $file = $PSScriptRoot + '/input10'
 $iterator = [System.IO.File]::ReadLines($file)
@@ -29,18 +29,14 @@ $score = 0
 $stack = [System.Collections.Stack]::new() 
 $iterator.ForEach({
     Write-Debug $_
-    foreach ($c in $_.ToCharArray())
-    {
-      if ( ('(', '<', '[', '{') -contains $c)
-      {
+    foreach ($c in $_.ToCharArray()) {
+      if ( ('(', '<', '[', '{') -contains $c) {
         $stack.Push($c)
       }
-      else
-      {
-        if (-not $stack.Count -or [math]::Abs($stack.Pop() - $c) -gt 2)
-        {
+      else {
+        if (-not $stack.Count -or [math]::Abs($stack.Pop() - $c) -gt 2) {
           $score += $error_score[$c]
-          Write-Debug $c
+          Write-Debug "corrupt $c with stack.Count $($stack.Count)" 
           break
         }
       }
@@ -49,3 +45,43 @@ $iterator.ForEach({
   })
 Write-Warning $score
 
+<#--- Part Two ---
+Now, discard the corrupted lines. The remaining lines are incomplete.
+You can only use closing characters (), ], }, or >), and you must add them in the correct order 
+so that only legal pairs are formed and all chunks end up closed.
+
+It's true! The score is determined by considering the completion string character-by-character. 
+Start with a total score of 0. Then, for each character, multiply the total score by 5 and 
+then increase the total score by the point value given for the character in the following table:
+
+    ): 1 point.
+    ]: 2 points.
+    }: 3 points.
+    >: 4 points.
+
+Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score? #>
+
+$scores = [System.Collections.Stack]::new() 
+$iterator = [System.IO.File]::ReadLines($file)
+$error_score = @{[char]'(' = 1; [char]'[' = 2; [char]'{' = 3; [char]'<' = 4; }
+$iterator.ForEach({
+    Write-Debug $_
+    $discard = $false
+    foreach ($c in $_.ToCharArray()) {
+      if ( ('(', '<', '[', '{') -contains $c) {
+        $stack.Push($c)
+      }
+      else {
+        if (-not $stack.Count -or [math]::Abs($stack.Pop() - $c) -gt 2) {   
+          $discard = $true
+          break
+        }
+      }
+    }
+    if (-not $discard) {
+      $scores.Push(($stack.ForEach({ $error_score[$_] }) | reduce { 5 * $a + $b }))
+      Write-Debug $scores.Peek()
+    }
+    $stack.Clear()
+  })
+Write-Warning ($scores | Sort-Object)[(($scores.Count - 1) / 2)]

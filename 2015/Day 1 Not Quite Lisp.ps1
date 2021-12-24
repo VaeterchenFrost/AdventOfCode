c755f556fed718876939b1aef40e71e5acc1852a
Martin Röbke <martin.roebke@web.de>
2021-12-24 02:13:21 +0100

    Update Day 1 Not Quite Lisp.ps1

--- 2015/Day 1 Not Quite Lisp.ps1
+++ 2015/Day 1 Not Quite Lisp.ps1
@@ -10,3 +10,14 @@ $c = $text.ToCharArray()
 $counter = 0
 (1..$text.Length).foreach({ $counter += 1 - 2 * ($c[$_ - 1] -eq ')'); if ($counter -lt 0) { Write-Warning $_; break } })
 Write-Warning $counter
+
+<# --- Part Two ---
+
+Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the basement? #>

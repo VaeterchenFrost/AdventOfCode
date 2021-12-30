13bc0a8655aa738ce4d4f319ea43b82b47ff3de0
Martin Röbke <martin.roebke@web.de>
2021-12-30 22:27:29 +0100

    Create Day 2 I Was Told There Would Be No Math.ps1

---
+++ 2015/Day 2 I Was Told There Would Be No Math.ps1
@@ -0,0 +1,31 @@
+<# --- Day 2: I Was Told There Would Be No Math ---
+https://adventofcode.com/2015/day/2
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
+
+For example:
+
+    A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+    A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+#>
+
+$year, $day = 2015, 2
+
+$inputfile = $PSScriptRoot + "/input${day}"
+if (-not ($lines = Get-Content $inputfile)) {
+    $request = Invoke-WebRequest -Uri "https://adventofcode.com/${year}/day/${day}/input" -Headers @{Cookie = "session=$env:ADVENTOFCODE_SESSION"; Accept = 'text/plain' }
+    Write-Debug "Got $($request.Headers.'Content-Length') Bytes"  
+    Out-File -FilePath $inputfile -InputObject $request.Content.Trim()
+    $lines = Get-Content $inputfile
+}
+
+$wrapping_paper = $lines | ForEach-Object { $ar = $_.Split('x') | ForEach-Object { [int]$_ } | Sort-Object; 3 * $ar[0] * $ar[1] + 2 * $ar[0] * $ar[2] + 2 * $ar[1] * $ar[2] } | Measure-Object -Sum
+Write-Warning $wrapping_paper.Sum
+
+<# --- Part Two --- How many total feet of ribbon should they order? #>
+
+$ribbon = $lines | ForEach-Object { $ar = $_.Split('x') | ForEach-Object { [int]$_ } | Sort-Object; 2 * ($ar[0] + $ar[1]) + $ar[0] * $ar[1] * $ar[2] } | Measure-Object -Sum
+Write-Warning $ribbon.Sum

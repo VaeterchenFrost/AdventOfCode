dbc4939ee04a92dd88b00c687c87289d60191b03
Martin Röbke <martin.roebke@web.de>
2021-12-31 02:12:05 +0100

    Update Day 3 Binary Diagnostic.ps1

--- 2021/Day 3 Binary Diagnostic.ps1
+++ 2021/Day 3 Binary Diagnostic.ps1
@@ -25,13 +25,9 @@ Use the binary numbers in your diagnostic report to calculate the gamma rate and
 
 $year, $day = 2021, 3
 
-$inputfile = $PSScriptRoot + "/input${day}"
-if (-not ($lines = Get-Content $inputfile)) {
-    $request = Invoke-WebRequest -Uri "https://adventofcode.com/${year}/day/${day}/input" -Headers @{Cookie = "session=$env:ADVENTOFCODE_SESSION"; Accept = 'text/plain' }
-    Write-Debug "Got $($request.Headers.'Content-Length') Bytes"  
-    Out-File -FilePath $inputfile -InputObject $request.Content.Trim()
-    $lines = Get-Content $inputfile
-}
+. "$PSScriptRoot/../scripts/LoadAocInput.ps1"
+$inputfile = $PSScriptRoot + "/input${day}" -replace '\\', '/'
+$lines = load_aoc_input $year $day $inputfile
 
 $instructions = $lines
 $stringlength = $instructions[0].Length
@@ -85,6 +81,6 @@ foreach ($index in (1..($stringlength - 1))) {
     }
     Write-Debug $co2_scrubber_rating.count
 }
-Write-Debug $oxygen_generator_rating
-Write-Debug $co2_scrubber_rating
-Write-Warning ([Convert]::ToInt32($oxygen_generator_rating, 2) * [Convert]::ToInt32($co2_scrubber_rating, 2))
+Write-Debug $oxygen_generator_rating[0]
+Write-Debug $co2_scrubber_rating[0]
+Write-Warning ([Convert]::ToInt32($oxygen_generator_rating[0], 2) * [Convert]::ToInt32($co2_scrubber_rating[0], 2))

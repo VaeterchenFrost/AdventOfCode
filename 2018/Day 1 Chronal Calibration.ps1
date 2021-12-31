ba7f6100835bf403f4fc6308f1a9087b0df6bb26
Martin Röbke <martin.roebke@web.de>
2021-12-31 02:08:32 +0100

    Update Day 1 Chronal Calibration.ps1

--- 2018/Day 1 Chronal Calibration.ps1
+++ 2018/Day 1 Chronal Calibration.ps1
@@ -5,13 +5,9 @@ Starting with a frequency of zero, what is the resulting frequency after all of 
 
 $year, $day = 2018, 1
 
-$inputfile = $PSScriptRoot + "/input${day}"
-if (-not ($lines = Get-Content $inputfile)) {
-    $request = Invoke-WebRequest -Uri "https://adventofcode.com/${year}/day/${day}/input" -Headers @{Cookie = "session=$env:ADVENTOFCODE_SESSION"; Accept = 'text/plain' }
-    Write-Debug "Got $($request.Headers.'Content-Length') Bytes"  
-    Out-File -FilePath $inputfile -InputObject $request.Content.Trim()
-    $lines = Get-Content $inputfile
-}
+. "$PSScriptRoot/../scripts/LoadAocInput.ps1"
+$inputfile = $PSScriptRoot + "/input${day}" -replace '\\', '/'
+$lines = load_aoc_input $year $day $inputfile
 
 $seen = New-Object 'System.Collections.Generic.HashSet[int]'
 $c = $frequency = 0

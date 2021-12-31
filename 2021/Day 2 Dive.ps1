65e609dbf7c8a7f843f966b6b9908385b48f0c81
Martin Röbke <martin.roebke@web.de>
2021-12-31 02:12:08 +0100

    Update Day 2 Dive.ps1

--- 2021/Day 2 Dive.ps1
+++ 2021/Day 2 Dive.ps1
@@ -19,13 +19,9 @@ After following these instructions, you would have a horizontal position of 15 a
 
 $year, $day = 2021, 2
 
-$inputfile = $PSScriptRoot + "/input${day}"
-if (-not ($lines = Get-Content $inputfile)) {
-    $request = Invoke-WebRequest -Uri "https://adventofcode.com/${year}/day/${day}/input" -Headers @{Cookie = "session=$env:ADVENTOFCODE_SESSION"; Accept = 'text/plain' }
-    Write-Debug "Got $($request.Headers.'Content-Length') Bytes"  
-    Out-File -FilePath $inputfile -InputObject $request.Content.Trim()
-    $lines = Get-Content $inputfile
-}
+. "$PSScriptRoot/../scripts/LoadAocInput.ps1"
+$inputfile = $PSScriptRoot + "/input${day}" -replace '\\', '/'
+$lines = load_aoc_input $year $day $inputfile
 
 $horizontal = $depth = 0
 

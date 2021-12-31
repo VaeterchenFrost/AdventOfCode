b40b7395dc607fc8964ce7f91dd93f17789c1e94
Martin Röbke <martin.roebke@web.de>
2021-12-31 01:59:27 +0100

    Update loading in 2015
    #6

--- 2015/Day 2 I Was Told There Would Be No Math.ps1
+++ 2015/Day 2 I Was Told There Would Be No Math.ps1
@@ -14,13 +14,9 @@ All numbers in the elves' list are in feet. How many total square feet of wrappi
 
 $year, $day = 2015, 2
 
-$inputfile = $PSScriptRoot + "/input${day}"
-if (-not ($lines = Get-Content $inputfile)) {
-    $request = Invoke-WebRequest -Uri "https://adventofcode.com/${year}/day/${day}/input" -Headers @{Cookie = "session=$env:ADVENTOFCODE_SESSION"; Accept = 'text/plain' }
-    Write-Debug "Got $($request.Headers.'Content-Length') Bytes"  
-    Out-File -FilePath $inputfile -InputObject $request.Content.Trim()
-    $lines = Get-Content $inputfile
-}
+. "$PSScriptRoot/../scripts/LoadAocInput.ps1"
+$inputfile = $PSScriptRoot + "/input${day}" -replace '\\', '/'
+$lines = load_aoc_input $year $day $inputfile
 
 $wrapping_paper = $lines | ForEach-Object { $ar = $_.Split('x') | ForEach-Object { [int]$_ } | Sort-Object; 3 * $ar[0] * $ar[1] + 2 * $ar[0] * $ar[2] + 2 * $ar[1] * $ar[2] } | Measure-Object -Sum
 Write-Warning $wrapping_paper.Sum

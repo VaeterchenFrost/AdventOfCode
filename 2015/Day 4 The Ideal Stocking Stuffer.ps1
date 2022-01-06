15e3c2cebabe18fb500f6d8c00c6943dc96c14fd
Martin Röbke <martin.roebke@web.de>
2022-01-06 22:49:30 +0100

    Update Day 4 The Ideal Stocking Stuffer.ps1

--- 2015/Day 4 The Ideal Stocking Stuffer.ps1
+++ 2015/Day 4 The Ideal Stocking Stuffer.ps1
@@ -4,6 +4,9 @@ Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gi
 
 To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes. The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal. To mine AdventCoins, 
 you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+0.25s for part 1
+8.17s for part 2
 #>
 
 $s = 'ckczppom'
@@ -13,7 +16,9 @@ $md5 = [System.Security.Cryptography.MD5]::Create()
 
 while (++$i) {
     $bytes = $md5.ComputeHash([System.Text.Encoding]::ASCII.GetBytes($s + $i))
-    if ($bytes[0] -eq 0 -and $bytes[1] -eq 0 -and $bytes[2] -lt 16) { break }
+    # if ($bytes[0] -eq 0 -and $bytes[1] -eq 0 -and $bytes[2] -lt 16) { break }
+    <# --- Part Two ---  Now find one that starts with six zeroes.  #>
+    if ($bytes[0] -eq 0 -and $bytes[1] -eq 0 -and $bytes[2] -eq 0) { break }
 }
 
 Write-Warning $i
